216477ca5a097dda0a0b2d43ed4aaa20993dc3e7
vladachugay <chugay.vlada@gmail.com>
2021-04-22 18:58:24 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import {RouterModule} from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+
+const routs = [
+  { path: '', component: HomePageComponent},
+  { path: 'contacts', component: ContactsComponent}
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routs)
   ],
   providers: [],
   bootstrap: [AppComponent]
